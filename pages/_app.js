68aea393e5773cf1687c71cd1a1213b9d7afa273
uddiptojana10 <janauddipto@gmail.com>
2025-09-27 22:00:24 +0530

    Set site title and description

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,15 @@
-import '../styles/globals.css'; export default function App({ Component, pageProps }) { return <Component {...pageProps} /> }
+import '../styles/globals.css';
+import Head from 'next/head';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Progress Tracker</title>
+        <meta name="description" content="Track your weekly progress across seasons" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
